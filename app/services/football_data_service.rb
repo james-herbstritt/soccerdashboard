ae62526d3573ca45350818ad61d049d9e71ca954
James Herbstritt <james.herbstritt@gmail.com>
2020-09-28 05:38:29 -0400

    Set env variable

--- app/services/football_data_service.rb
+++ app/services/football_data_service.rb
@@ -19,10 +19,10 @@ class FootballDataService
 
   def available_leagues
     response = HTTParty.get('https://api.football-data.org/v2/competitions/',
-                            headers: { 'X-Auth-Token' => 'f8457569bf914dda8d3fc3111de6289c' })
+                            headers: { 'X-Auth-Token' => ENV['FOOTBALL_AUTH_TOKEN'] })
     competitions = response['competitions']
 
-    available_leagues = competitions.select { |comp| @leagues_ids.include?(comp['id']) }
+    available_leagues = competitions.select { |comp| @league_ids.include?(comp['id']) }
     available_leagues.sort_by { |league| league['name'] }
   end
 end
